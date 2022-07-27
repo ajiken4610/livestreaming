54bf06daed521ff7db536ebb3f3a9206708708e8
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-07-27 09:23:43 +0900

    jsに変更

--- server/server.js
+++ server/server.js
@@ -1,29 +1,29 @@
-import admin from "firebase-admin";
-import firestore from "firebase-admin/firestore";
-import ws from "ws";
+const admin = require("firebase-admin");
+const firestore = require("firebase-admin/firestore");
+const ws = require("ws");
 var Server = ws.Server;
 var webSocket = new Server({ port: 3050 });
 var serviceAccount = require("C:\\Users\\user\\Documents\\serviceaccountkey\\livestreamingstate-firebase-adminsdk-t37gc-20b1bf10ee.json");
 var app = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount),
 });
 var db = firestore.getFirestore(app);
 console.log(db);
 var doc = db.doc("state/yuki");
 var currentState = {};
 var unsub = doc.onSnapshot((snapshot) => {
-    var data = snapshot.data();
-    currentState = data;
-    console.log(data);
-    webSocket.clients.forEach((client) => {
-        client.send(JSON.stringify(data));
-    });
+  var data = snapshot.data();
+  currentState = data;
+  console.log(data);
+  webSocket.clients.forEach((client) => {
+    client.send(JSON.stringify(data));
+  });
 });
 webSocket.on("connection", (client) => {
-    client.send(JSON.stringify(currentState));
+  client.send(JSON.stringify(currentState));
 });
 // 後始末
 process.on("exit", (exitCode) => {
-    unsub();
+  unsub();
 });
 process.on("SIGINT", () => process.exit(0));
